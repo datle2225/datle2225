96c7547cbf974fa7bd641436e7f9f9a2bdf8d978
datle2225 <datle2225@gmail.com>
2019-12-27 18:36:56 +0700

    Meo Hoang: Add footer(html, css - done)

---
+++ MeoHoang/assets/js/index.js
@@ -0,0 +1,63 @@
+//body
+
+$("header").load("header.html nav", () => {
+    let deactiveIcon = $el => {
+        $el.children(".title").children(".activeIcon").removeClass("show");
+        $el.children(".title").children(".defaultIcon").removeClass("hide");
+        $el.children(".title").children(".hoverIcon").removeClass("hide");
+        $el.children(".nav-subnav").removeClass("show");
+    }
+    
+    $(".title").on("click", function() {
+        $(this).parent().siblings().removeClass("blueColor");
+        deactiveIcon($(this).parent().siblings());
+        $(this).parent().toggleClass("blueColor");
+        if (window.innerWidth > 991) $(this).children(".activeIcon").toggleClass("show");
+        else $(this).not(".adopt").children(".activeIcon").toggleClass("show");
+        $(this).children(".defaultIcon").toggleClass("hide");
+        $(this).children(".hoverIcon").toggleClass("hide");
+        if (window.innerWidth > 991) $(this).siblings(".nav-subnav").toggleClass("show");
+        else $(this).siblings(".nav-subnav:not(.adopt)").toggleClass("show");
+    })
+    
+    $(".hamburger-menu").on("click", function() {
+        $("header").toggleClass("blueBackground");
+        $(".whiteLogo").toggleClass("show");
+        $(".blackLogo").toggleClass("hide");
+        $(".activeIcon-hamburger").toggleClass("show");
+        $(".defaultIcon-hamburger").toggleClass("hide");
+        $(".nav-menu:not('.hamburger-menu')").slideToggle("fast");
+    })
+    
+    $("#nav-item-search").children("svg").on("click", () => {
+        $("#nav-item-search-active").addClass("show-flex");
+        deactiveIcon($(".nav-item"));
+        $(".nav-item").removeClass("blueColor");
+    })
+    
+    $("svg.search-article-close").on("click", () => {
+        $("#nav-item-search-active").removeClass("show-flex");
+    })
+    
+    // $("svg.search-article").on("click", () => {
+    //     $("#nav-item-search-active").removeClass("show-flex");
+    // })
+});
+
+$("footer").load("footer.html div.footer", () => {
+    let deactiveArrow = $el => {
+        console.log($el.parent().siblings().children(".footer-item-arrow").children(".activeArrow"))
+        $el.parent().siblings().children(".footer-item-arrow").children(".activeArrow").removeClass("show");
+        $el.parent().siblings().children(".footer-item-arrow").children(".defaultArrow").removeClass("hide");
+        $el.parent().siblings().children(".list").slideUp("fast");
+    }
+    
+    $(".footer-item-arrow").on("click", function() {
+        if (window.innerWidth < 992) {
+            deactiveArrow($(this));
+            $(this).children(".activeArrow").toggleClass("show");
+            $(this).children(".defaultArrow").toggleClass("hide");
+            $(this).siblings(".list").slideToggle("fast");
+        }
+    })
+});
